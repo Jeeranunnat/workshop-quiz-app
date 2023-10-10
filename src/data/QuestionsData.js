7e04fb235435f4c6e858268272368b7d503c33e5
Jeeranunnat <jeeranun.work@gmail.com>
2023-10-10 23:35:41 +0700

    git init

---
+++ src/data/QuestionsData.js
@@ -0,0 +1,43 @@
+const QuestionsData = [
+  {
+    question: 'ข้อใดไม่ใช่ tag HTML',
+    A: '<kong>',
+    B: '<a>',
+    C: '<p>',
+    D: '<h1>',
+    answer: 'A',
+  },
+  {
+    question: 'ข้อใดถูกต้องเกี่ยวกับ React',
+    A: 'ทำงานฝั่ง server',
+    B: 'ใช้จัดการฐานข้อมูล',
+    C: 'เป็น Javascript Library',
+    D: 'ถูกทุกข้อ',
+    answer: 'C',
+  },
+  {
+    question: 'ข้อใดคือชื่อ Browser',
+    A: 'PHP',
+    B: 'React',
+    C: 'Mysql',
+    D: 'Chrome',
+    answer: 'D',
+  },
+  {
+    question: 'Border Style เป็นการกำหนดคุณสมบัติส่วนใด',
+    A: 'เส้นขอบ',
+    B: 'พื้นหลัง',
+    C: 'ขนาดข้อความ',
+    D: 'ถูกทุกข้อ',
+    answer: 'A',
+  },
+  {
+    question: 'ข้อใดคือ Semantic Element ใน HTML',
+    A: '<article>',
+    B: '<footer>',
+    C: '<section>',
+    D: 'ถูกทุกข้อ',
+    answer: 'D',
+  },
+];
+export default QuestionsData;
